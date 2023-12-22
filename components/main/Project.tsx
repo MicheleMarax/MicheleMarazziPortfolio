69b94c7d24407dc6ebb8d999eab03d5a0f29705b
Michele <michelemarazzi01@gmail.com>
2023-12-22 15:54:00 +0100

    New projects added with '#' will be ignored, and an empty message aborts the commit.

--- components/main/Project.tsx
+++ components/main/Project.tsx
@@ -19,61 +19,90 @@ const Projects = () => {
           The focus is on implementing advanced algorithms for dynamic 3D mesh creation to enhance visual appeal and gameplay experiences in video games.
           The project covers algorithmic exploration, optimization, integration with game mechanics, user interaction, and visual aesthetics.
           The goal is to showcase practical applications and demonstrate the impact of procedural techniques on game development and interactive applications."
-        />        
-       <ProjectCard
+          tags={["C#", "Unity 3D"]}
+        />
+        <ProjectCard
           src="/SaveThePlanet.png"
           title="Project save the planet"
           description="This software engineering test project involves developing an Android game in Unity, focusing on implementing SOLID principles.
           The game showcases single responsibility, open/closed, Liskov substitution, interface segregation, and dependency inversion principles for a robust and maintainable codebase. 
           Through this project, the student demonstrates proficiency in Unity, Android game development, and mastery of SOLID principles in software engineering."
-        />    
+          tags={["C#", "Unity 3D"]}
+        />
         <ProjectCard
           src="/CryptoWallet.png"
           title="Crypto wallet tracker"
           description="For a university test, this Android app, developed in Java using Android Studio, functions as a Crypto Wallet tracker. 
           It fetches real-time cryptocurrency values from a third-party server via REST API. 
           With a user-friendly interface, the app enables users to manage their crypto assets and stay updated on market trends."
-        />    
+          tags={["Java", "Android studio", "Rest API", "Sql"]}
+        />
       </div>
       <div className="h-[300] w-[300] flex flex-col md:flex-row gap-10 px-10 py-10">
         <ProjectCard
           src="/PortfolioSite.png"
           title="My portfolio site"
           description="My portfolio website, developed in visual code to learn react js."
-        />     
-        
-      <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] flex-[1] z-[21] opacity-0">
-        <Image
-          src="/PortfolioSite.png"
-          alt=""
-          width={1080}
-          height={1080}
-          className="w-full object-contain"
+          tags={["React", "Js", "Html", "CSS"]}
         />
 
-        <div className="relative p-4">
-          <h1 className="text-2xl font-semibold text-white"></h1>
-          <p className="mt-2 text-gray-300"></p>
-        </div>
-      </div>
-
-      <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] flex-[1] z-[21] opacity-0">
-        <Image
-          src="/PortfolioSite.png"
-          alt=""
-          width={1080}
-          height={1080}
-          className="w-full object-contain"
+        <ProjectCard
+          src="/NoderyImg.png"
+          title="Nodery"
+          description="Nodery is a sleek and addictive puzzle game developed in Unity 2D using C#. 
+          Your mission: connect nodes strategically to solve intricate puzzles. With intuitive controls and a visually stunning design, 
+          Nodery offers a satisfying challenge for players of all levels.
+          Immerse yourself in the art of connection and enjoy a minimalist yet vibrant gaming experience in Nodery."
+          tags={["C#", "Unity 2D"]}
         />
 
-        <div className="relative p-4">
-          <h1 className="text-2xl font-semibold text-white"></h1>
-          <p className="mt-2 text-gray-300"></p>
+        <ProjectCard
+          src="/VrSpaceShooter.png"
+          title="Vr Space Shooter"
+          description="A prototype VR space game where you have to drive a spaceship and fight against endless hordes of enemies.
+          Developed for meta quest 2 in Unity and written in C#."
+          tags={["C#", "Unity 3D", "VR", "Meta quest 2"]}
+        />
+      </div>
+
+      <div className="h-[300] w-[300] flex flex-col md:flex-row gap-10 px-10 py-10">
+        <ProjectCard
+          src="/TheFlameImg.png"
+          title="The flame"
+          description="2D platform rage quit game like super mario, developed in Unity for high school programming competition."
+          tags={["C#", "Unity 2D", "Team working"]}
+        />
+
+        <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] flex-[1] z-[21] opacity-0">
+          <Image
+            src="/PortfolioSite.png"
+            alt=""
+            width={1080}
+            height={1080}
+            className="w-full object-contain"
+          />
+
+          <div className="relative p-4">
+            <h1 className="text-2xl font-semibold text-white"></h1>
+            <p className="mt-2 text-gray-300"></p>
+          </div>
+        </div>
+
+        <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] flex-[1] z-[21] opacity-0">
+          <Image
+            src="/PortfolioSite.png"
+            alt=""
+            width={1080}
+            height={1080}
+            className="w-full object-contain"
+          />
+
+          <div className="relative p-4">
+            <h1 className="text-2xl font-semibold text-white"></h1>
+            <p className="mt-2 text-gray-300"></p>
+          </div>
         </div>
       </div>
-        
-      </div>
-      
     </div>
   );
 };
--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -5,9 +5,10 @@ interface Props {
   src: string;
   title: string;
   description: string;
+  tags: string[];
 }
 
-const ProjectCard = ({ src, title, description }: Props) => {
+const ProjectCard = ({ src, title, description, tags }: Props) => {
   return (
     <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] flex-[1] z-[21]">
       <Image
@@ -17,6 +18,14 @@ const ProjectCard = ({ src, title, description }: Props) => {
         height={1080}
         className="w-full object-contain"
       />
+
+      <div className="w-full flex flex-row items-center">
+        {tags.map((tag) => (
+          <div className="Tag-box py-[5px] px-[10px] border border-[#7042f88b] opacity-[0.9] ">
+            <h1 className="welcome-text text-[13px] text-[#b49bff]">{tag}</h1>
+          </div>
+        ))}
+      </div>
 
       <div className="relative p-4">
         <h1 className="text-2xl font-semibold text-white">{title}</h1>
